f2c2278d733a8f74d4ae395cc984d0840dea0223
sjeongab <sjeongab@connect.ust.hk>
2022-05-03 15:27:53 +0800

    HowToPlay

--- Code/Configuration.js
+++ Code/Configuration.js
@@ -1,4 +1,4 @@
-const time = 5;
+const time = 40;
 const numItemUpper = 20;
 const numItemLower = 10;
 const catchRange = 1.8;
---
+++ Code/js/HowToPlay.js
@@ -0,0 +1,9 @@
+function instruction(){
+    //console.log('hello');
+    document.getElementById("howtoplay").style.display = "inline-block";
+}
+/*
+function close(){
+    console.log('close!');
+    document.getElementById("howtoplay").style.display = "none";
+}*/
